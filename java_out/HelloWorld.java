918d88340aa59dd78c58733502b29e2fed681fd4
Yogeshwar <132248362+Yogesh8551@users.noreply.github.com>
2026-01-07 15:17:32 +0530

    this is changes in script'

--- java_out/HelloWorld.java
+++ java_out/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World from VB!");
         System.out.println("Hello, My name is yogeshwar!");
+        System.out.println("Hello, i am done successfully!");
     }
 }
 ```
--- vb_src/HelloWorld.vb
+++ vb_src/HelloWorld.vb
@@ -2,5 +2,6 @@ Module HelloWorld
     Sub Main()
         Console.WriteLine("Hello, World from VB!")
         Console.WriteLine("Hello, My name is yogeshwar!")
+        Console.WriteLine("Hello, i am done successfully!")
     End Sub
 End Module
