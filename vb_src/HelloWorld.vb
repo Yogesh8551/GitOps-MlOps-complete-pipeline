c26661725083351cf651bc8eb83b8fe41bcdd94a
Yogesh8551 <132248362+Yogesh8551@users.noreply.github.com>
2026-01-07 16:52:15 +0530

    Remove duplicate success message in HelloWorld

--- vb_src/HelloWorld.vb
+++ vb_src/HelloWorld.vb
@@ -3,5 +3,6 @@ Module HelloWorld
         Console.WriteLine("Hello, World from VB!")
         Console.WriteLine("Hello, My name is yogeshwar!")
         Console.WriteLine("Hello, i am done successfully!")
+        Console.WriteLine("Hello, i am done successfully!")
     End Sub
 End Module
