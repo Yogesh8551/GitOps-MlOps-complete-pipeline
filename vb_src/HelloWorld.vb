e6823ec4577c4c9fe4b7cd6ecb71d5a0d8ef85a9
Yogeshwar <132248362+Yogesh8551@users.noreply.github.com>
2026-01-07 19:41:56 +0530

    this is my commit 4567 789

--- vb_src/HelloWorld.vb
+++ vb_src/HelloWorld.vb
@@ -27,7 +27,7 @@ Module HelloWorld
         ElseIf op = "*" Then
             Console.WriteLine("Result: " & (a * b))
         ElseIf op = "/" Then
-            Console.WriteLine("Result: " & (a / b) #bracket is not their)7689
+            Console.WriteLine("Result: " & (a / b))
         Else
             Console.WriteLine("Invalid operator!")
         End If
