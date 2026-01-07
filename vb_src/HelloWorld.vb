e485ee09c7bb30b58b65f650083dbd2dcfade537
Yogeshwar <132248362+Yogesh8551@users.noreply.github.com>
2026-01-07 19:45:17 +0530

    this is changes in vb94784987654321

--- vb_src/HelloWorld.vb
+++ vb_src/HelloWorld.vb
@@ -5,6 +5,11 @@ Module HelloWorld
         Console.WriteLine("Hello, i am done successfully!")
         Console.WriteLine("Hello, i am done successfully!")
         Console.WriteLine("Hello, i am done successfully!")
+        onsole.WriteLine("Hello, World from VB!")
+        Console.WriteLine("Hello, My name is yogeshwar!")
+        Console.WriteLine("Hello, i am done successfully!")
+        Console.WriteLine("Hello, i am done successfully!")
+        Console.WriteLine("Hello, i am done successfully!")
 
         Module Calculator
     Sub Main()
